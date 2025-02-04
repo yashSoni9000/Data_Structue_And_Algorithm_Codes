6cf88b3e8f5796e4ff19fb2f18856b5fce56af7f
yash <ys77357@gmail.com>
2025-02-04 14:23:53 +0530

    added reverse intger problem

---
+++ Basic_Maths/Reverse_Integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+        int count = 0, num =Math.abs(x);
+        while(num != 0){
+            int lastDigit = num % 10;
+            if ((count > (Integer.MAX_VALUE - lastDigit) / 10)) return 0;
+            count = (count * 10) + lastDigit;
+            num /= 10;
+        }
+        return (x > 0) ? count : (-count);
+    }
+}
