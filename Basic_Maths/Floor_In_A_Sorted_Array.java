dcf267a23d39befe35da7690838335222dd946a6
yash <ys77357@gmail.com>
2025-03-17 16:58:38 +0530

    updated code in floor in a sorted array

--- Basic_Maths/Floor_In_A_Sorted_Array.java
+++ Basic_Maths/Floor_In_A_Sorted_Array.java
@@ -1,18 +1,17 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-        int ans = -1;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
-                break;
-            }
-            else if (nums[mid] < target) low = mid + 1;
+    static int findFloor(int[] arr, int x) {
+        // write code here
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        
+        while (low < high) {
+            int mid = low + ((high - low + 1) / 2);
+            
+            if (arr[mid] <= x) low = mid;
             else high = mid - 1;
         }
-
-        return ans;
+        
+        return (arr[low] <= x) ? low : -1;
     }
 }
