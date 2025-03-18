6ae530fbae4410eaeeec21a352becd0351ff0efc
yash <ys77357@gmail.com>
2025-03-18 21:24:16 +0530

    search insert postion added

---
+++ Basic_Maths/Search_Insert_Position.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + ((high - low + 1) / 2);
+            if (nums[mid] <= target) low = mid;
+            else high = mid - 1;
+        }
+
+        if (nums[low] >= target) return low;
+        else return low + 1;
+    }
+}
