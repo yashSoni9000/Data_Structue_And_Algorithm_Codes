cedc92222c15ef300b5fa0409ebe4116981f4880
yash <ys77357@gmail.com>
2025-03-20 14:12:13 +0530

    search in rotated array added

---
+++ Basic_Maths/Search_In_Rotated_Array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length - 1;
+        int low = 0, high = n;
+        int pivot = 0;
+
+        while (low < high) {
+            int mid = low + ((high - low) / 2);
+
+            if(nums[mid] > nums[n]) low = mid + 1;
+            else high = mid;
+        }
+        pivot = low;
+
+        low = 0;
+        high = n;
+
+        while (low <= high) {
+            int mid = low + ((high - low + 1) / 2);
+            
+            int curr = (mid + pivot) % nums.length;
+            if (nums[curr] == target) return curr;
+            if(nums[curr] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return -1;
+    }
+}
