f171e92c6dd7d2692e76d77b56e4378e27c8d623
yash <ys77357@gmail.com>
2025-03-28 00:25:41 +0530

    koko eating bananas added

---
+++ Basic_Maths/Koko_Eating_Bananas.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        return naiveSolution(piles, h);
+
+        // return optimalSolution(piles, h);
+    }
+
+    // public int optimalSolution(int[] piles, int h) {
+    // }
+
+    public int naiveSolution(int[] piles, int h) {
+        int low = 1, high = 1000000000;
+        int minAns = high;
+
+        while (low < high) {
+            int ans = 0;
+            int mid = low + ((high - low) / 2);
+
+            for (int pile : piles) ans += Math.ceil((double)pile / (double)mid);
+
+            if (ans <= h) {
+                minAns = mid;
+                high = mid;
+            }
+            else low = mid + 1;
+        }
+
+        return minAns;
+    }
+}
