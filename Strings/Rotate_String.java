63f04871aaa7e7b4b547263cbf2e135f5f993ab4
yash <ys77357@gmail.com>
2025-04-12 23:37:52 +0530

    Rotate String added

---
+++ Strings/Rotate_String.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+
+        String combinedString = s + s;
+        if (combinedString.contains(goal)) return true;
+        
+        return false;
+    }
+}
