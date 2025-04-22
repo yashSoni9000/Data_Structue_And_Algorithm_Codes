f6a0b0f71577430efa2503edba80dffe69ce6ab8
yash <ys77357@gmail.com>
2025-04-22 10:16:08 +0530

    better approach added

--- Strings/Longest_Palindromic_Substring.java
+++ Strings/Longest_Palindromic_Substring.java
@@ -1,5 +1,38 @@
 class Solution {
     public String longestPalindrome(String s) {
+        // return naiveSolution(s);
+
+        return betterSolution(s);
+    }
+
+    public String betterSolution(String s) {
+        if (s.length() <= 1) return s;
+
+        String longestPalindrome = s.substring(0,1);
+        int longestPalindromeLen = longestPalindrome.length();
+
+        for (int i = 0; i < s.length(); i++) {
+            String odd = midStart(s, i, i); // if palindrome is odd
+            String even = midStart(s, i, i + 1); // if palindrome is even
+
+            if (odd.length() > longestPalindrome.length()) longestPalindrome = odd;
+            if (even.length() > longestPalindrome.length()) longestPalindrome = even;
+        }
+
+        return longestPalindrome;
+    }
+
+    public String midStart(String s, int start, int end) {
+        while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            start--;
+            end++;
+        }
+
+        return s.substring(start + 1, end);
+    }
+
+    // naive approach
+    public String naiveSolution(String s) {
         int strLen = s.length();
 
         for (int i = strLen ; i > 0; i--) {
