2342c1624ccfbe519ed86837aae350967b2c5c50
yash <ys77357@gmail.com>
2025-04-23 09:43:57 +0530

    Longest common prefix added

---
+++ Strings/Longest_Common_Prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        StringBuilder ans = new StringBuilder("");
+
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != last.charAt(i)) break;
+
+            ans.append(first.charAt(i));
+        }
+
+        return ans.toString();
+    }
+}
