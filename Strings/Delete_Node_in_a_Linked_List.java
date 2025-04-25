8456afa8c623848593c16eb662ae5c20fda9b375
yash <ys77357@gmail.com>
2025-04-25 09:41:33 +0530

    Delete node in a linked list added

---
+++ Strings/Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void deleteNode(ListNode node) {
+        // naiveApproach(node);
+
+        optimalApproach(node);
+    }
+
+    public void optimalApproach(ListNode node) {
+        node.val = node.next.val;
+
+        node.next = node.next.next;
+    }
+
+    public void naiveApproach(ListNode node) {
+        ListNode mover = node;
+        ListNode nextData = node.next;
+
+        while (nextData.next != null) {
+            mover.val = nextData.val;
+            mover = nextData;
+            nextData = nextData.next;
+        }
+        mover.val = nextData.val;
+        mover.next = null;
+    }
+}
