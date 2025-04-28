c7f10f5c19e2a31b98ed80fd0f7694f0613cac69
yash <ys77357@gmail.com>
2025-04-28 10:05:12 +0530

    added recursive approach in reverse linked list

--- Linked_List/Reverse_Linked_List.java
+++ Linked_List/Reverse_Linked_List.java
@@ -12,7 +12,22 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) return head;
         
-        return iterativeApproach(head);
+        // return iterativeApproach(head);
+
+        return recursiveApproach(head, null);
+    }
+
+    public ListNode recursiveApproach(ListNode current, ListNode prev) {
+        if (current == null) return prev;
+
+        // next = current.next;
+        // current.next = prev;
+        // prev = current;
+        // current = next;
+        ListNode next = current.next;
+        current.next = prev;
+
+        return recursiveApproach(next, current);
     }
 
     public ListNode iterativeApproach(ListNode head) {
@@ -27,4 +42,4 @@ class Solution {
 
         return curr1;
     }
-}
+}`
