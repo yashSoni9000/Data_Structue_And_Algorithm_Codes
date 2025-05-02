73d3a9fc6e37652d962791d3d5f0ce01500e1062
yash <ys77357@gmail.com>
2025-05-02 09:34:27 +0530

    added optimised code

--- Linked_List/Palindrome_Linked_List.java
+++ Linked_List/Palindrome_Linked_List.java
@@ -11,23 +11,67 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         if (head.next == null) return true;
-
-        return naiveApproach(head);
-    }
-
-    // naive approach
-    public boolean naiveApproach(ListNode head) {
-        boolean skip = false;
         ListNode mover = head;
-        int length = 0, half;
+        int length = 0;
 
         while (mover != null) {
             length++;
             mover = mover.next;
         }
 
+        // return naiveApproach(head, length);
+
+        return optimalApproach(head, length);
+    }
+
+    // optimal approach
+    public boolean optimalApproach(ListNode head, int length) {
+        int half = length / 2;
+        boolean skip = false;
+
+        if (length % 2 == 1) skip = true;
+
+        ListNode[] reversedHead = reverseFirstHalf(head, half);
+
+        ListNode first = reversedHead[0];
+        ListNode second = reversedHead[1];
+
+        if (skip) second = second.next;
+
+        for (int i = 0; i < half; i++) {
+            if (first.val != second.val) return false;
+
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+
+    // helper function for reversing first half of linked list
+    public ListNode[] reverseFirstHalf(ListNode head, int half) {
+        ListNode prev = null, current = head, next;
+        ListNode[] nodes = new ListNode[2];
+
+        for (int i = 0; i < half; i++) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        nodes[0] = prev;
+        nodes[1] = current;
+
+        return nodes;
+    }
+
+
+    // naive approach
+    public boolean naiveApproach(ListNode head, int length) {
+        boolean skip = false;
+        int half;
+
         Stack<Integer> st = new Stack<>();
-        mover = head;
+        ListNode mover = head;
         half = length / 2;
 
         if (length % 2 == 1) skip = true;
