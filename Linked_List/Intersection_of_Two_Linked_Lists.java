fd65f4ddd3d20b66632081a09bbd8b3815390615
yash <ys77357@gmail.com>
2025-05-10 23:45:07 +0530

    added optimised code for intersection of two linked lists

--- Linked_List/Intersection_of_Two_Linked_Lists.java
+++ Linked_List/Intersection_of_Two_Linked_Lists.java
@@ -9,10 +9,46 @@
  *     }
  * }
  */
-class Solution {
+public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        return naiveApproach(headA, headB);
+        // return naiveApproach(headA, headB);
+
+        return optimalApproach(headA, headB);
     }
+
+    public ListNode optimalApproach(ListNode headA, ListNode headB) {
+        ListNode moverA = headA, moverB = headB, ans;
+        int countA = 1, countB = 1;
+
+        while (moverA.next != null) {
+            moverA = moverA.next;
+            countA++;
+        }
+        while (moverB.next != null) {
+            moverB = moverB.next;
+            countB++;
+        }
+
+        if (moverA != moverB) return null;
+
+        if (countA > countB) ans = getCommonNode(countA - countB, headA, headB);
+        else ans = getCommonNode(countB - countA, headB, headA);
+
+        return ans;
+    }
+
+    public ListNode getCommonNode(int diff, ListNode headA, ListNode headB) {
+        for (int i = 0; i < diff; i++) headA = headA.next;
+        // System.out.println(headA.val);
+
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+
+        return headA;
+    }
+
     public ListNode naiveApproach(ListNode headA, ListNode headB) {
         Set<ListNode> st = new HashSet<>();
         ListNode moverA = headA, moverB = headB;
@@ -26,7 +62,7 @@ class Solution {
             moverA = moverA.next;
             moverB = moverB.next;
         }
-
+        
         while (moverA != null) {
             if (st.contains(moverA)) return moverA;
             moverA = moverA.next;
