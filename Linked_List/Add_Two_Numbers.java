a0ba6eae2fc486d98786f3fda93db37774efa30f
yash <ys77357@gmail.com>
2025-05-13 23:41:48 +0530

    added optimised code with help of add two numbers

--- Linked_List/Add_Two_Numbers.java
+++ Linked_List/Add_Two_Numbers.java
@@ -10,9 +10,32 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return ugliestApproach(l1, l2);
+        // return ugliestApproach(l1, l2);
 
-        // return optimalApproach(l1, l2);
+        return optimalApproach(l1, l2);
+    }
+
+    public ListNode optimalApproach(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode mover = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int currSum = x + y + carry;
+            carry = currSum / 10;
+            int digit = currSum % 10;
+
+            mover.next = new ListNode(digit);
+            mover = mover.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        return dummy.next;
     }
 
     public ListNode ugliestApproach(ListNode l1, ListNode l2) {
