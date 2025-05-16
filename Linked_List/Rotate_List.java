c6e36144393b6eed2ab94afdece744eaadc4460f
yash <ys77357@gmail.com>
2025-05-16 09:27:21 +0530

    Added optimal code for rotate list

--- Linked_List/Rotate_List.java
+++ Linked_List/Rotate_List.java
@@ -12,6 +12,33 @@ class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null) return head;
 
+        // return naiveApproach(head, k);
+
+        return optimalApproach(head, k);
+    }
+    public ListNode optimalApproach(ListNode head, int k) {
+        ListNode mover = head;
+        int length = 1;
+
+        while (mover.next != null) {
+            mover = mover.next;
+            length++;
+        }
+
+        mover.next = head;
+        // mover = head;  // if we had used this we would have to do '-1' in loop below
+
+        k %= length;
+        for (int i = 0; i < (length - k); i++) mover = mover.next;
+
+        head = mover.next;
+        mover.next = null;
+
+        return head;
+    }
+
+    // naive approach
+    public ListNode naiveApproach(ListNode head, int k) {
         ListNode mover = head;
         int length = 1;
         while (mover.next != null) {
