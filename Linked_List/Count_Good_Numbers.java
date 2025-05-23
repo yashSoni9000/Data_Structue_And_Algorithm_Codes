40025775502ae06daca50942e17f808896a4489a
yash <ys77357@gmail.com>
2025-05-23 23:54:33 +0530

    Count good numbers added

---
+++ Linked_List/Count_Good_Numbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    long MOD = 1_000_000_007L;
+
+    public int countGoodNumbers(long n) {
+        long even = (n + 1) / 2; 
+        long odd = n / 2;        
+
+        return (int)((pows(5, even) * pows(4, odd)) % MOD);
+    }
+
+    public long pows(long x, long n) {
+        if (n == 0) return 1;
+
+        long half = pows((x * x) % MOD, n / 2);
+        if (n % 2 == 0) {
+            return half;
+        }
+        return (half * x) % MOD;
+    }
+}
