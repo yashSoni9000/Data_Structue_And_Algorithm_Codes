0ed757ab4eef42981cffdd91dc5a2647e285ba09
yash <ys77357@gmail.com>
2025-05-31 23:26:01 +0530

    revised code for generate parentheses

--- Recursion/Generate_Parentheses.java
+++ Recursion/Generate_Parentheses.java
@@ -1,14 +1,13 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-
         helper(ans, 0, 0, "", n);
 
         return ans;
     }
 
     public void helper(List<String> ans, int left, int right, String s, int n) {
-        if (s.length() == 2 * n) {
+        if (s.length() == (2 * n)) {
             ans.add(s);
             return;
         }
