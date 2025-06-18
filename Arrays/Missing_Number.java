46f46826505fbeff4b7b345426e1df7d8ff84ee4
yash <ys77357@gmail.com>
2025-06-18 22:37:09 +0530

    Missing number added

---
+++ Arrays/Missing_Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int ans = 0;
+        for (int i = 1; i <= nums.length; i++) ans ^= i;
+
+        for (int i = 0; i < nums.length; i++) ans ^= nums[i];
+
+        return ans;
+    }
+}
