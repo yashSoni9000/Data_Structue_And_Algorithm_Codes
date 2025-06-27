81ed6cb23ed32805591d85ed488ad64bb55d5f02
yash <ys77357@gmail.com>
2025-06-27 23:57:14 +0530

    maximum subarray added

---
+++ Arrays/Maximum_Subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // int ans = Integer.MIN_VALUE;
+        // for (int i = 0; i < nums.length; i++) {
+        //     int count = 0;
+        //     for (int j = i; j < nums.length; j++) {
+        //         count += nums[j];
+        //         ans = Math.max(count, ans);
+        //     }
+        // }
+
+        // return ans;
+
+        long sum = 0, max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > max) max = sum;
+
+            if (sum < 0) sum = 0;
+        }
+
+        return (int)max;
+    }
+}
