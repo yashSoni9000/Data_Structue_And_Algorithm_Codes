4561fe30c5fb9d68763961ef18a83f5d96150e43
yash <ys77357@gmail.com>
2025-07-03 23:40:00 +0530

    added optimal solution for rearrange array element by sign

--- Arrays/Rearrange_Array_Elements_by_Sign.java
+++ Arrays/Rearrange_Array_Elements_by_Sign.java
@@ -1,5 +1,30 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        // return naiveApproach(nums);
+
+        return optimalSolution(nums);
+    }
+
+    public int[] optimalSolution(int[] nums) {
+        int even = 0, odd = 1;
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                ans[odd] = nums[i];
+                odd += 2; 
+            }
+            else {
+                ans[even] = nums[i];
+                even += 2;
+            }
+        }
+
+        return ans;
+    }
+
+    public int[] naiveApproach(int[] nums) {
         int n = nums.length;
         int[] neg = new int[n / 2];
         int[] pos = new int[n / 2];
