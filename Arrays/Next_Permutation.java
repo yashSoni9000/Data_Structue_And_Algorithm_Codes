b2859e7f7fbd733bbb1f8f9fd28b7ab3fcd57fe6
yash <ys77357@gmail.com>
2025-07-05 23:55:41 +0530

    added my code for next permutation

--- Arrays/Next_Permutation.java
+++ Arrays/Next_Permutation.java
@@ -1,37 +1,27 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        if (n <= 1) return;
-
-        // Step 1: Find the first pair from right where nums[i-1] < nums[i]
-        int i = n - 1;
-        while (i > 0 && nums[i-1] >= nums[i]) {
-            i--;
+        if (n == 1) return;
+        int i = n - 1, j = n - 1;
+        
+        while (i > 0 && (nums[i] <= nums[i - 1])) i--;
+        if (i == 0) {
+            reverse(0, n - 1, nums);
+            return;
         }
+        while (j != i && (nums[j] <= nums[i - 1])) j--;
 
-        // Step 2: If such a pair is found, find the smallest number on the right side
-        // that is greater than nums[i-1] and swap them
-        if (i > 0) {
-            int j = n - 1;
-            while (j >= i && nums[j] <= nums[i-1]) {
-                j--;
-            }
-            swap(nums, i-1, j);
-        }
-
-        // Step 3: Reverse the subarray from i to the end
-        reverse(nums, i, n-1);
+        swap(j, i - 1, nums);
+        reverse(i, n - 1, nums);
     }
 
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public void reverse(int start, int end, int[] nums) {
+        while (start <= end) swap(start++, end--, nums);
     }
 
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start++, end--);
-        }
+    public void swap(int first, int second, int[] nums) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
