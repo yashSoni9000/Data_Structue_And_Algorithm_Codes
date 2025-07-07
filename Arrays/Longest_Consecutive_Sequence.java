d542b02db3f01847c630888aadaa05c20250e108
yash <ys77357@gmail.com>
2025-07-07 23:47:41 +0530

    added better solution for longest consecutive sequence

--- Arrays/Longest_Consecutive_Sequence.java
+++ Arrays/Longest_Consecutive_Sequence.java
@@ -1,46 +1,25 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        return subOptimal(nums);
-
-        // return optimal(nums);
-    }
-
-    public int optimal(int[] nums) {
+        if (nums.length == 0) return 0;
+        
+        Set<Integer> hs = new HashSet<>();
         int ans = 0;
-        HashSet<Integer> hs = new HashSet<>();
+
         for (int num : nums) hs.add(num);
 
-        for (int num : nums) {
-            if (!hs.contains(num - 1)) {
-                int temp = ++num;
-                int freq = 1;
-                while (hs.contains(temp++)) freq++;
+        for (int curr : hs) {
+            if (!hs.contains(curr - 1)) {
+                int count = 0;
+                int next = curr;
 
-                ans = Math.max(ans, freq);
+                while (hs.contains(next)) {
+                    count++;
+                    next++;
+                }
+                ans = Math.max(count, ans);
             }
         }
 
-        return ans;
-    }
-
-    public int subOptimal(int[] nums) {
-        int ans = 0;
-        Map<Integer,Integer> mp = new HashMap<>();
-
-        for (int num : nums) {
-            if (!mp.containsKey(num)) {
-                int left = (mp.containsKey(num - 1) ? mp.get(num - 1) : 0);
-                int right = (mp.containsKey(num + 1) ? mp.get(num + 1) : 0);
-
-                int sum = left + right + 1;
-                mp.put(num, sum);
-
-                ans = Math.max(ans, sum);
-
-                mp.put(num - left, sum);
-                mp.put(num + right, sum);
-            }
-        }
         return ans;
     }
 }
