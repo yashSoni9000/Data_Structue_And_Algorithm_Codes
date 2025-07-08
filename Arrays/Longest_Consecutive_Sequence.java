7561d7607b158904ec5f9d95a737c1f8a47ea4c2
yash <ys77357@gmail.com>
2025-07-08 23:40:40 +0530

    added my code for longest consecutive sequence

--- Arrays/Longest_Consecutive_Sequence.java
+++ Arrays/Longest_Consecutive_Sequence.java
@@ -1,7 +1,5 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) return 0;
-        
         Set<Integer> hs = new HashSet<>();
         int ans = 0;
 
@@ -9,14 +7,12 @@ class Solution {
 
         for (int curr : hs) {
             if (!hs.contains(curr - 1)) {
-                int count = 0;
-                int next = curr;
-
-                while (hs.contains(next)) {
+                int temp = curr + 1,  count = 1;
+                while (hs.contains(temp)) {
                     count++;
-                    next++;
+                    temp++;
                 }
-                ans = Math.max(count, ans);
+                ans = Math.max(ans, count);
             }
         }
 
